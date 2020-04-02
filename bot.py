2669e78d5e9943aa25c1047b6755243a8b116b0b
Max <am.ra2016@yandex.ru>
2020-04-02 15:24:25 +0300

    base

--- bot.py
+++ bot.py
@@ -1,2 +1,19 @@
 import telebot
 
+bot = telebot.TeleBot('1077053623:AAE8yg9jrRas7h7mTgKaNQAjOTeIsgwJHGI')
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
+
+
+bot.polling()
