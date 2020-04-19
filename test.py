f8bddf539ea1736f3c573f6a0ae571664cfdc849
Max <am.ra2016@yandex.ru>
2020-04-19 12:34:03 +0300

    dev2

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
-a = (None, )
-if a[0]:
-    print(1)
+from random import randint
+
+
+def f(n, tf):
+    print(n)
+    if tf:
+        a = [0, 1, 2, 3, 4, 5]
+        for i in a:
+            if randint(1, 100) < 10:
+                f(i, True)
+            else:
+                f(i, False)
+        a.clear()
+
+
+f(-1, True)
